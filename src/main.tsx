d156523f7bdbf2425eed1fb2a9164ab2b29c631f
Samuel <kamarsamuel9@gmail.com>
2025-03-01 23:35:38 -0500

    added react-router

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "@/components/ui/provider";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
 import App from "./App.tsx";
@@ -12,7 +13,11 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Provider>
---
+++ src/route.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function router() {
+  return (
+    <div>route</div>
+  )
+}
+
+export default router
